954ce4273ae0af0a8e86b2992e019967060b7653
Cameron Ford <c.r.ford@outlook.com>
2022-05-24 12:10:23 -0600

    1st commit 1. Console must greet player with a fun message  2. Console must ask for the player's name and store it. steps are complet and the while loop is there but has no content

---
+++ index.js
@@ -0,0 +1,10 @@
+const readline = require("readline-sync");
+console.log("hey im glad you came to play this game!")
+const name = readline.question("What is your name? ");
+console.log("HEY "+name+" to walk use W")
+let alive = true
+while (alive = true) {
+
+}
+// run program = node (file name)git commit -m "1st commit 1. Console must greet player with a fun message
+2. Console must ask for the player's name and store it. steps are complet and the while loop is there with no content 
